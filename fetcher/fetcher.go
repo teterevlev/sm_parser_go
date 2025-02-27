b07f39903aa0fde350ffb9f508f27f265e9f4336
oleg <oleg>
2025-02-27 16:56:35 -0300

    added headers+cookies to FetchHTML

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -11,10 +11,28 @@ type HttpClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-func FetchHTML(ctx context.Context, client HttpClient, url string) (string, error) {
+type FetchConfig struct {
+	Headers map[string]string
+	Cookies map[string]string
+}
+
+func FetchHTML(ctx context.Context, client HttpClient, url string, config ...FetchConfig) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
 	if err != nil {
 		return "", fmt.Errorf("request creation err: %w", err)
+	}
+
+	// Если конфигурация передана, добавляем заголовки и куки
+	if len(config) > 0 {
+		// Добавляем заголовки
+		for key, value := range config[0].Headers {
+			req.Header.Add(key, value)
+		}
+
+		// Добавляем куки
+		for key, value := range config[0].Cookies {
+			req.AddCookie(&http.Cookie{Name: key, Value: value})
+		}
 	}
 
 	resp, err := client.Do(req)
